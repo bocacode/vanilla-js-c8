186de550921db9afb75d049fd41587ed249dacbe
Todd Albert <todd@bocacode.com>
2022-10-25 15:30:17 -0400

    getting coffees from SampleAPIs

---
+++ script.js
@@ -0,0 +1,31 @@
+function displayDate() {
+  document.getElementById('demo').innerHTML = Date()
+}
+document.getElementsByTagName('button')[0]
+  .addEventListener("click", ()=>{
+    alert('You clicked the button')
+  })
+
+window.addEventListener('resize', () => {
+  document.getElementById('demo').innerHTML = "YOU RESIZED THE WINDOW!"
+})
+
+window.addEventListener("keydown", (event) => {
+  switch(event.key) {
+    case 'v':
+      document.body.style.backgroundColor = 'violet'
+      break
+    case 'w':
+      document.body.style.backgroundColor = 'white'
+      break
+    case 's':
+      document.body.style.backgroundColor = 'salmon'
+      break
+    case 'o':
+      document.body.style.backgroundColor = 'orange'
+      break
+    case 'g':
+      document.body.style.backgroundColor = 'grey'
+      break
+  }
+})
